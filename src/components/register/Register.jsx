3c419c0060b6e4659b3d91121644e71b4652b5e2
HyeonJu <hhjc017@gmail.com>
2022-06-16 01:05:11 +0900

    Add Register

---
+++ src/components/register/Register.jsx
@@ -0,0 +1,15 @@
+import './Register.css';
+import React from 'react';
+
+const Register = () => {
+  return (
+    <section className="register-container">
+      <h1 className="register-title">계정이 없으신가요?</h1>
+      <a className="register-link" href="#">
+        가입하기
+      </a>
+    </section>
+  );
+};
+
+export default Register;
